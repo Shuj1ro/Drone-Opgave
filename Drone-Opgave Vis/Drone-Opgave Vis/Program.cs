cfd2387b01fd18dbf291b889f5fcf7ef6dfc496f
Shuj1ro <David.fyhn.jonas@gmail.com>
2023-08-18 07:54:18 +0200

    The thingy yes

---
+++ Drone-Opgave Vis/Drone-Opgave Vis/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Drone_Opgave_Vis
+{
+    using System.IO;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string theShit = File.ReadAllText("flyvning1.csv");
+                Console.WriteLine(theShit);
+        }
+    }
+}
