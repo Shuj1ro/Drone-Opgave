0ef880a6d402f14fbfbf35ce87912e8f7e1c28c6
Shuj1ro <David.fyhn.jonas@gmail.com>
2023-08-23 10:59:29 +0200

    Trying to skip lines
    Forsøgte at skippe linjer i filen

--- Drone-Opgave Vis/Drone-Opgave Vis/Program.cs
+++ Drone-Opgave Vis/Drone-Opgave Vis/Program.cs
@@ -1,6 +1,9 @@
-﻿namespace Drone_Opgave_Vis
+﻿using System;
+using System.Linq;
+using System.IO;
+namespace Drone_Opgave_Vis
 {
-    using System.IO;
+    
     internal class Program
     {
         static void Main(string[] args)
@@ -13,13 +16,24 @@
             // using the method
             String[] strlist = theShit.Split(spearator, count,
                    StringSplitOptions.RemoveEmptyEntries);
+            
 
             foreach (String s in strlist)
             {
                 Console.WriteLine(s);
+                File.AppendAllText("Sorted.csv", s + "/n");
             }
 
            
+            string[] lines = new string[s.Length];
+            for (int i = 0; i <lines.Length; i++)
+            {
+
+                lines[i] = s;
+
+            }
+
+
 
         }
     }
